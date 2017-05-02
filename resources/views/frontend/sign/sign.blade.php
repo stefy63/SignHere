40eb0559e5fef365d7ec2444d66bdfece1cfde89
stefano.sca <stefano.sca@gmail.com>
2017-05-02 15:47:20 +0200

    bugfix test

--- resources/views/frontend/sign/sign.blade.php
+++ resources/views/frontend/sign/sign.blade.php
@@ -3,7 +3,7 @@
 <script src="{{ asset('js/pdf.js') }}"></script>
 <script src="{{ asset('js/compatibility.js') }}"></script>
 
-<script src="{{ asset('js/jspdf.min.js') }}"></script>
+<!--<script src="{{ asset('js/jspdf.min.js') }}"></script>-->
 @endpush
 @section('content')
 <div class="row">
@@ -20,25 +20,25 @@
             </div>
             <hr>
             <div class="ibox-content col-lg-12">
-                <div>
+                <div style="height: 100px;" class="pull-left">
+
                   <button id="prev">Previous</button>
                   <button id="next">Next</button>
                   &nbsp; &nbsp;
                   <span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
-                </div>
-                <div style="height: 200px;"> class="pull-right"
 
-                    <canvas id="canvas" width="200px" height="100px"></canvas>
 
-                    <object id="sigCtl1" style="width:80mm;height:345mm"
-                            type="application/x-florentis-signature">
-                    </object>
-                     <div  style="padding: 10px 50px;">
-                        <input type="button" value="Sign" style="height:10mm;width:35mm" onclick="Capture()"
-                        title="Starts signature capture" />
-                     </div>
+                    <canvas id="pdf-canvas" width="100px" height="100px"></canvas>
+
                 </div>
                 <div class="pull-right">
+                    <object id="sigCtl1" style="width:80mm;height:45mm"
+                            type="application/x-florentis-signature">
+                    </object>
+                    <div  style="padding: 10px 50px;">
+                        <input type="button" value="Sign" style="height:10mm;width:35mm" onclick="Capture()"
+                               title="Starts signature capture" />
+                    </div>
                     <br/>
                     <textarea cols="30" rows="10" id="txtSignature"></textarea>
                 </div>
@@ -56,7 +56,7 @@ $(function () {
         pageRendering = false,
         pageNumPending = null,
         scale = 0.8,
-        canvas = document.getElementById('canvas'),
+        canvas = document.getElementById('pdf-canvas'),
         ctx = canvas.getContext('2d');
 
     /**
@@ -143,38 +143,39 @@ $(function () {
     });
 
 ////////// WACOM
+    var Licence = 'AgAkAMlv5nGdAQVXYWNvbQ1TaWduYXR1cmUgU0RLAgOBAgJkAACIAwEDZQA';
+
     try {
-          print("CLEAR");
+          //print("CLEAR");
           var sigCtl = document.getElementById("sigCtl1");
+          SigCtl.Licence = Licence;
           sigCtl.BackStyle = 1;
           sigCtl.DisplayMode=0; // fit signature to control
-          print("Checking components...");
+          //print("Checking components...");
           var sigcapt = new ActiveXObject('Florentis.DynamicCapture');  // force 'can't create object' error if components not yet installed
           var lic = new ActiveXObject("Wacom.Signature.Licence");
-          print("DLL: Licence.dll   v" + lic.GetProperty("Component_FileVersion"));
-          print("DLL: flSigCOM.dll  v" +   sigCtl.GetProperty("Component_FileVersion"));
-          print("DLL: flSigCapt.dll v" + sigcapt.GetProperty("Component_FileVersion"));
-          print("Test application ready.");
-          print("Press 'Sign' to capture a signature.");
+          //print("DLL: Licence.dll   v" + lic.GetProperty("Component_FileVersion"));
+          //print("DLL: flSigCOM.dll  v" +   sigCtl.GetProperty("Component_FileVersion"));
+          //print("DLL: flSigCapt.dll v" + sigcapt.GetProperty("Component_FileVersion"));
+          //print("Test application ready.");
+          //print("Press 'Sign' to capture a signature.");
         }
         catch(ex) {
           Exception("OnLoad() error: " + ex.message);
         }
 
-        var Licence = 'AgAkAMlv5nGdAQVXYWNvbQ1TaWduYXR1cmUgU0RLAgOBAgJkAACIAwEDZQA';
 
         function Capture() {
         try {
-          print("Capturing signature...");
+          //print("Capturing signature...");
           var sigCtl = document.getElementById("sigCtl1");
-          SigCtl.Licence = Licence;
           var dc = new ActiveXObject("Florentis.DynamicCapture");
           var rc = dc.Capture(sigCtl, "{{$document->name}}", "{{$document->dossier->name}}");
           if(rc != 0 )
-            print("Capture returned: " + rc);
+            //print("Capture returned: " + rc);
           switch( rc ) {
             case 0: // CaptureOK
-              print("Signature captured successfully");
+              //print("Signature captured successfully");
               var txtSignature = document.getElementById("txtSignature");
               flags = 0x2000 + 0x80000 + 0x400000; //SigObj.outputBase64 | SigObj.color32BPP | SigObj.encodeData
               b64 = sigCtl.Signature.RenderBitmap("", 300, 150, "image/png", 0.5, 0xff0000, 0xffffff, 0.0, 0.0, flags );
@@ -183,25 +184,25 @@ $(function () {
               document.getElementById("b64image").src=imgSrcData;
               break;
             case 1: // CaptureCancel
-              print("Signature capture cancelled");
+              //print("Signature capture cancelled");
               break;
             case 100: // CapturePadError
-              print("No capture service available");
+              //print("No capture service available");
               break;
             case 101: // CaptureError
-              print("Tablet Error");
+              //print("Tablet Error");
               break;
             case 102: // CaptureIntegrityKeyInvalid
-              print("The integrity key parameter is invalid (obsolete)");
+              //print("The integrity key parameter is invalid (obsolete)");
               break;
             case 103: // CaptureNotLicensed
-              print("No valid Signature Capture licence found");
+              //print("No valid Signature Capture licence found");
               break;
             case 200: // CaptureAbort
-              print("Error - unable to parse document contents");
+              //print("Error - unable to parse document contents");
               break;
             default:
-              print("Capture Error " + rc);
+              //print("Capture Error " + rc);
               break;
           }
         }
