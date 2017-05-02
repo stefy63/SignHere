d3574c20d90fb95582c31ce3d0829ecfd9a60048
stefano.sca <stefano.sca@gmail.com>
2017-05-02 16:19:52 +0200

    bugfix

--- resources/views/frontend/sign/sign.blade.php
+++ resources/views/frontend/sign/sign.blade.php
@@ -20,29 +20,55 @@
             </div>
             <hr>
             <div class="ibox-content col-lg-12">
-                <div style="height: 100px;" class="pull-left">
+                <div style="height: 500px;overflow-y: auto" class="pull-left col-lg-6">
 
-                  <button id="prev">Previous</button>
-                  <button id="next">Next</button>
+                  <button id="prev" class="col-lg-6">Previous</button>
+                  <button id="next" class="col-lg-6">Next</button>
                   &nbsp; &nbsp;
                   <span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
 
 
-                    <canvas id="pdf-canvas" width="100px" height="100px"></canvas>
+                    <canvas id="pdf-canvas" class="pull-left" ></canvas>
 
                 </div>
-                <div class="pull-right">
-                    <object id="sigCtl1" style="width:80mm;height:45mm"
-                            type="application/x-florentis-signature">
-                    </object>
-                    <div  style="padding: 10px 50px;">
-                        <input type="button" value="Sign" style="height:10mm;width:35mm" onclick="Capture()"
-                               title="Starts signature capture" />
-                        <br>
-                        <img id="b64image" style="width:300px;height:150px"></img>
+                <div class="pull-right col-lg-6">
+                    <div>
+                        <h2>Test Signature Control</h2>
+                        <div id="not_ie_warning" style="display:none">
+                            <h2>WARNING:</h2>
+                            This application is only supported by Internet Explorer<br/>
+                            (The Javascript uses ActiveX controls which are not supported by alternative browsers such as Firefox)<br/>
+                        </div>
+                        <table style="padding: 10px 90px;">
+                            <tr>
+                                <td rowspan="3">
+                                    <div>
+                                        <object id="sigCtl1" style="width:60mm;height:35mm"
+                                                type="application/x-florentis-signature">
+                                        </object>
+                                    </div>
+                                </td>
+                                <td  style="padding: 10px 50px;">
+                                    <input type="button" value="Start" style="height:10mm;width:35mm" onclick="Capture()"
+                                           title="Starts signature capture" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 10px 50px;">
+                                    <input type="button" value="About" style="height:10mm;width:35mm" onclick="AboutBox()"
+                                           title="Displays the Help About box" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 10px 50px;">
+                                    <input type="button" value="Info" style="height:10mm;width:35mm" onclick="DisplaySignatureDetails()"
+                                           title="Displays the signature details" />
+                                </td>
+                            </tr>
+                        </table>
+                        <br/>
+                        <textarea cols="50" rows="15" id="txtDisplay"></textarea>
                     </div>
-                    <br/>
-                    <textarea cols="30" rows="10" id="txtSignature"></textarea>
                 </div>
             </div>
         </div>
@@ -145,77 +171,116 @@ $(function () {
     });
 
 ////////// WACOM
-    var Licence = 'AgAkAMlv5nGdAQVXYWNvbQ1TaWduYXR1cmUgU0RLAgOBAgJkAACIAwEDZQA';
-     function OnLoad() {
-         try {
-             //print("CLEAR");
-             var sigCtl = document.getElementById("sigCtl1");
-             sigCtl.Licence = Licence;
-             sigCtl.BackStyle = 1;
-             sigCtl.DisplayMode = 0; // fit signature to control
-             //print("Checking components...");
-             var sigcapt = new ActiveXObject('Florentis.DynamicCapture');  // force 'can't create object' error if components not yet installed
-             var lic = new ActiveXObject("Wacom.Signature.Licence");
-             //print("DLL: Licence.dll   v" + lic.GetProperty("Component_FileVersion"));
-             //print("DLL: flSigCOM.dll  v" +   sigCtl.GetProperty("Component_FileVersion"));
-             //print("DLL: flSigCapt.dll v" + sigcapt.GetProperty("Component_FileVersion"));
-             //print("Test application ready.");
-             //print("Press 'Sign' to capture a signature.");
-         }
-         catch (ex) {
-             Exception("OnLoad() error: " + ex.message);
-         }
-     }
 
-        function Capture() {
+    var Licence = 'AgAkAMlv5nGdAQVXYWNvbQ1TaWduYXR1cmUgU0RLAgOBAgJkAACIAwEDZQA';
+    function Capture() {
         try {
-          //print("Capturing signature...");
-          var sigCtl = document.getElementById("sigCtl1");
-          var dc = new ActiveXObject("Florentis.DynamicCapture");
-          //var rc = dc.Capture(sigCtl, "{{$document->name}}", "{{$document->dossier->name}}");
-          var rc = dc.Capture(sigCtl, "who", "why");
-          if(rc != 0 )
-            //print("Capture returned: " + rc);
-          switch( rc ) {
-            case 0: // CaptureOK
-              //print("Signature captured successfully");
-              var txtSignature = document.getElementById("txtSignature");
-              flags = 0x2000 + 0x80000 + 0x400000; //SigObj.outputBase64 | SigObj.color32BPP | SigObj.encodeData
-              b64 = sigCtl.Signature.RenderBitmap("", 300, 150, "image/png", 0.5, 0xff0000, 0xffffff, 0.0, 0.0, flags );
-              txtSignature.value=b64;
-              var imgSrcData = "data:image/png;base64,"+b64;
-              document.getElementById("b64image").src=imgSrcData;
-              break;
-            case 1: // CaptureCancel
-              //print("Signature capture cancelled");
-              break;
-            case 100: // CapturePadError
-              //print("No capture service available");
-              break;
-            case 101: // CaptureError
-              //print("Tablet Error");
-              break;
-            case 102: // CaptureIntegrityKeyInvalid
-              //print("The integrity key parameter is invalid (obsolete)");
-              break;
-            case 103: // CaptureNotLicensed
-              //print("No valid Signature Capture licence found");
-              break;
-            case 200: // CaptureAbort
-              //print("Error - unable to parse document contents");
-              break;
-            default:
-              //print("Capture Error " + rc);
-              break;
-          }
+            print("Capturing signature...");
+            var sigCtl = document.getElementById("sigCtl1");
+            var dc = new ActiveXObject("Florentis.DynamicCapture");
+            var rc = dc.Capture(sigCtl, "who", "why");
+            if(rc != 0 )
+                print("Capture returned: " + rc);
+            switch( rc ) {
+                case 0: // CaptureOK
+                    print("Signature captured successfully");
+                    break;
+                case 1: // CaptureCancel
+                    print("Signature capture cancelled");
+                    break;
+                case 100: // CapturePadError
+                    print("No capture service available");
+                    break;
+                case 101: // CaptureError
+                    print("Tablet Error");
+                    break;
+                case 102: // CaptureIntegrityKeyInvalid
+                    print("The integrity key parameter is invalid (obsolete)");
+                    break;
+                case 103: // CaptureNotLicensed
+                    print("No valid Signature Capture licence found");
+                    break;
+                case 200: // CaptureAbort
+                    print("Error - unable to parse document contents");
+                    break;
+                default:
+                    print("Capture Error " + rc);
+                    break;
+            }
         }
         catch(ex) {
-          Exception("Capture() error: " + ex.message);
+            Exception("Capture() error: " + ex.message);
         }
-      }
+    }
 
+    function DisplaySignatureDetails() {
+        try {
+            var sigCtl = document.getElementById("sigCtl1");
+            if (sigCtl.Signature.IsCaptured) {
+                print("Signature Information:");
+                print("  Name:   " + sigCtl.Signature.Who);
+                print("  Date:   " + sigCtl.Signature.When);
+                print("  Reason: " + sigCtl.Signature.Why);
+            }
+            else
+            {
+                print("No signature captured");
+            }
+        }
+        catch(ex) {
+            Exception("DisplaySignatureDetails() error: " + ex.message);
+        }
+    }
 
-       OnLoad();
+    function AboutBox() {
+        try {
+            var sigCtl = document.getElementById("sigCtl1");
+            sigCtl.AboutBox();
+        }
+        catch(ex) {
+            Exception("About() error: " + ex.message);
+        }
+    }
+
+    function Exception(txt) {
+        print("Exception: " + txt);
+    }
+    function print(txt) {
+        var txtDisplay = document.getElementById("txtDisplay");
+        if(txt == "CLEAR" )
+            txtDisplay.value = "";
+        else {
+            txtDisplay.value += txt + "\n";
+            txtDisplay.scrollTop = txtDisplay.scrollHeight; // scroll to end
+        }
+    }
+
+    function OnLoad() {
+        try {
+            if( !("ActiveXObject" in window) ) {
+                document.getElementById("not_ie_warning").style.display="block";
+                return;
+            }
+            print("CLEAR");
+            var sigCtl = document.getElementById("sigCtl1");
+            //sigCtl.SetProperty("Licence","licence string");
+            sigCtl.BackStyle = 1;
+            sigCtl.DisplayMode=0; // fit signature to control
+            print("Checking components...");
+            var sigcapt = new ActiveXObject('Florentis.DynamicCapture');  // force 'can't create object' error if components not yet installed
+            var lic = new ActiveXObject("Wacom.Signature.Licence");
+            print("DLL: Licence.dll   v" + lic.GetProperty("Component_FileVersion"));
+            print("DLL: flSigCOM.dll  v" +   sigCtl.GetProperty("Component_FileVersion"));
+            print("DLL: flSigCapt.dll v" + sigcapt.GetProperty("Component_FileVersion"));
+            print("Test application ready.");
+            print("Press 'Start' to capture a signature.");
+        }
+        catch(ex) {
+            Exception("OnLoad() error: " + ex.message);
+        }
+    }
+
+    OnLoad();
 /////////////// JSPDF
 
 
