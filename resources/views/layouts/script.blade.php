8e02cf17cfd3c78faf67968942cd20bcb3e2912a
stefano.sca <stefano.sca@gmail.com>
2017-05-31 15:20:29 +0200

    fix

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -167,13 +167,19 @@ console.log(st);
 
 })
 
+    var lastScroll = 0;
     $(window).scroll(function(event){
         //Sets the current scroll position
         var st = $(this).scrollTop();
 
         //Determines up-or-down scrolling
+        if (st > lastScroll){
+            $(".footer").css("display",'inline')
+        }
         if(st == 0){
             $(".footer").css("display",'none')
         }
+        //Updates scroll position
+        lastScroll = st;
     });
 </script>
