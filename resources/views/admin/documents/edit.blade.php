acedbb22fcc5c1d31c5cd05722aba8ac444586c4
stefano.sca <stefano.sca@gmail.com>
2017-06-08 14:48:47 +0200

    fix

--- resources/views/admin/documents/edit.blade.php
+++ resources/views/admin/documents/edit.blade.php
@@ -177,9 +177,9 @@
 </div>
 
 <div id="showModal"></div>
-
+@push('scripts')
 <script>
-$(document).ready(function() {
+$(function() {
 
     var elem = document.querySelector('.js-switch-signed');
     var switchery = new Switchery(elem, { color: '#1AB394' });
@@ -222,4 +222,5 @@ $(document).ready(function() {
 
 })
 </script>
+@endpush
 @endsection
