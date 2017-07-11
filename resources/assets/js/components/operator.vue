da3430736871f2ff629feabfa362b377b65091ad
stefano.sca <stefano.sca@gmail.com>
2017-07-11 12:42:03 +0200

    test socket event

--- resources/assets/js/components/operator.vue
+++ resources/assets/js/components/operator.vue
@@ -73,11 +73,13 @@ module.exports = {
         stop_ask_response:function (message) {
             this.Call.splice($.inArray(message.userId, this.Call), 1 );
             this.numCall = this.Call.length;
-            console.log('response-check-user-connection......');
+            console.log('response-check-user-connection......'+message.userId);
         },
         other_response:function (message) {
-            //if(message.operatorId != this.suser)
-                this.stop_ask_response(message);
+            //if(message.operatorId != this.suser) this.stop_ask_response(message);
+            this.Call.splice($.inArray(message.userId, this.Call), 1 );
+            this.numCall = this.Call.length;
+            console.log('operator-notify-response......'+message.userId);
         },
         accept_call:function () {
             var that = this;
