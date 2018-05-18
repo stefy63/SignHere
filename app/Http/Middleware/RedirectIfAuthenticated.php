b8d52b175428f86c612baf5fce2bcfc6bb695f90
stefano.sca <stefano.sca@gmail.com>
2018-05-18 22:38:55 +0200

    Set error from VerifyCSRF token

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,6 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        die(Auth::guard($guard)->check());
         if (Auth::guard($guard)->check()) {
             return redirect('/');
         }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,8 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
-use Closure;
-use Illuminate\Support\Facades\Auth;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -16,26 +14,6 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
-
-
-    public function handle($request, Closure $next)
-    {
-        //die($response);
-        if (
-            $this->isReading($request) ||
-            $this->runningUnitTests() ||
-            $this->inExceptArray($request) ||
-            $this->tokensMatch($request)
-        ) {
-            return $this->addCookieToResponse($request, $next($request));
-        }
-
-        // redirect the user back to the last page and show error
-        return redirect('/home')->withErrors( [__('admin.sessione_expired')] );
-
-
-        return $next($request);
-    }
 
 }
 
