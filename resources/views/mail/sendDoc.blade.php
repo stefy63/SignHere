072ec2acfff5b6c01f0a1f4a78a2e68885c0c71f
stefano.sca <stefano.sca@gmail.com>
2018-06-28 12:07:26 +0200

    Adjust mail template

--- resources/views/mail/sendDoc.blade.php
+++ resources/views/mail/sendDoc.blade.php
@@ -1,5 +1,6 @@
 <div>
     <p>{!! __('sign.header_mail') !!}<span>{{$document->date_sign}}</span></p>
-    <p>{!! __('sign.body_mail') !!}<span>{{$brand->description}}</span></p>
-    <p>{!! __('sign.footer_mail') !!}<span>{{$client->surname}}  {{$client->name}}</span></p>
+    <p>{!! __('sign.body_mail') !!}</p>
+    <p>{!! __('sign.footer_mail') !!}<span>{{$sender->surname}}  {{$sender->name}}</span></p>
+    <p><span>{{$brand->description}}</span></p>
 </div>
